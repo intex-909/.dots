637de9b2f2ae41bb948f131b583019ca3c43a8fc
intex-909 <68182191+intex-909@users.noreply.github.com>
2022-08-28 10:55:30 +0000

    Update install.sh: Ensure .config dir exists

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+sudo -u $USER mkdir -p .config
 
 # install packages
 sudo apt install -y \
@@ -9,12 +12,18 @@ sudo apt install -y \
 	bat \
 	stow \
 
+
 # stow
 stow git
 stow zsh
 
+
+# add zsh to valid login shells
+command -v zsh | sudo tee -a /etc/shells
+
 # use zsh as default shell
 sudo chsh -s $(which zsh) $USER
+
 
 # install fnm
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir $HOME/.local/bin
